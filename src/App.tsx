e8e1de078c2f4277d759f7f70be88e880845a5ae
Nguyễn Dũng <admin@users-MacBook-Pro.local>
2024-04-26 18:53:20 +0700

    add com

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "antd";
+declare global {
+  interface window {
+    ReactNativeWebView: any;
+  }
+}
 function App() {
+  useEffect(() => {
+    const sendRandomMessage = () => {
+      window.postMessage(
+        JSON.stringify({ cookieId: `Hello anh em ${Math.random()}` }),
+        "*"
+      );
+    };
+
+    console.log("first111");
+    sendRandomMessage();
+    // const interval = setInterval(sendRandomMessage, 2000);
+
+    // return () => clearInterval(interval);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +37,17 @@ function App() {
         >
           Learn React
         </a>
+        <Button
+          onClick={() => {
+            console.log("first");
+            window.postMessage(
+              JSON.stringify({ cookieId: `Hello anh em ${Math.random()}` }),
+              "*"
+            );
+          }}
+        >
+          12312
+        </Button>
       </header>
     </div>
   );
